3c1b7031655adc55518f06380c1c37f7d3a8881e
seungyeon.lee <dus3214@gmail.com>
2022-01-08 14:18:46 +0900

    로그인 이메일 비밀번호 입력시 버튼 활성화 기능 추가

--- js/loginEmail.js
+++ js/loginEmail.js
@@ -1,6 +1,21 @@
+const emailField = document.querySelector('.email-field');
+const btn = document.getElementById('login-btn');
+const email = emailField.querySelector('#email');
+const pw = emailField.querySelector('#pw');
+
+// 이메일, 비밀번호 입력 값이 있다면 버튼 활성화 / 입력 값이 없다면 비활성화
+function keyupDisabled() {
+  if (email.value && pw.value) {
+    btn.classList.remove('btn-L--off');
+    btn.disabled = false;
+  } else {
+    btn.disabled = true;
+  }
+}
+
+// 버튼 클릭시 서버에 유저가 있다면 로컬스토리지에 저장 후 홈 피드 화면으로 이동
+// 유저가 아니라면 경고 텍스트가 나옵니다
 async function login() {
-  const email = document.querySelector("#email");
-  const pw = document.querySelector("#pw");
   const res = await fetch('http://146.56.183.55:5050/user/login', {
     method: 'POST',
     headers: {
@@ -15,27 +30,23 @@ async function login() {
   })
   const json = await res.json();
   const user = json.user;
+  console.log(user);
   if (user) {
     for (const key in user) {
       if (Object.hasOwnProperty.call(user, key)) {
         const data = user[key];
+        console.log(data);
         localStorage.setItem(key, data);
         location.href = '../pages/homeFeed/feedhome.html';
       }
     }
-
   } else { 
-    if (email.value === '' && pw.value === '') {
-      document.getElementById('pwAlert').textContent = '이메일 또는 비밀번호를 입력해주세요.';
-    } else if (email.value && pw.value === '') {
-      document.getElementById('pwAlert').textContent = '비밀번호를 입력해주세요.';
-    } else if (pw.value && email.value === '') {
-      document.getElementById('emailAlert').textContent = '이메일을 입력해주세요.';
-    } else {
-      document.getElementById('pwAlert').textContent =
-        '이메일이 또는 비밀번호가 일치하지 않습니다.';
-      pw.value = '';
-    }
+    document.getElementById('pwAlert').textContent =
+      '이메일이 또는 비밀번호가 일치하지 않습니다.';
+    btn.classList.add('btn-L--off');
+    pw.value = '';
   }
 }
-document.querySelector("#login-btn").addEventListener('click', login);
+
+emailField.addEventListener('keyup', keyupDisabled);
+btn.addEventListener('click', login);
