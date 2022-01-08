aabfead6417ebb98ff595d4321aed6fc4545d890
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-01-08 19:25:53 +0900

    create logout modal js

--- js/loginEmail.js
+++ js/loginEmail.js
@@ -1,12 +1,12 @@
-const emailField = document.querySelector('.email-field');
-const btn = document.getElementById('login-btn');
-const email = emailField.querySelector('#email');
-const pw = emailField.querySelector('#pw');
+const emailField = document.querySelector(".email-field");
+const btn = document.getElementById("login-btn");
+const email = emailField.querySelector("#email");
+const pw = emailField.querySelector("#pw");
 
 // 이메일, 비밀번호 입력 값이 있다면 버튼 활성화 / 입력 값이 없다면 비활성화
 function keyupDisabled() {
   if (email.value && pw.value) {
-    btn.classList.remove('btn-L--off');
+    btn.classList.remove("btn-L--off");
     btn.disabled = false;
   } else {
     btn.disabled = true;
@@ -16,10 +16,10 @@ function keyupDisabled() {
 // 버튼 클릭시 서버에 유저가 있다면 로컬스토리지에 저장 후 홈 피드 화면으로 이동
 // 유저가 아니라면 경고 텍스트가 나옵니다
 async function login() {
-  const res = await fetch('http://146.56.183.55:5050/user/login', {
-    method: 'POST',
+  const res = await fetch("http://146.56.183.55:5050/user/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       user: {
@@ -27,7 +27,7 @@ async function login() {
         password: pw.value,
       },
     }),
-  })
+  });
   const json = await res.json();
   const user = json.user;
   console.log(user);
@@ -37,16 +37,16 @@ async function login() {
         const data = user[key];
         console.log(data);
         localStorage.setItem(key, data);
-        location.href = '../pages/homeFeed/feedhome.html';
+        location.href = "../pages/home.html";
       }
     }
-  } else { 
-    document.getElementById('pwAlert').textContent =
-      '이메일이 또는 비밀번호가 일치하지 않습니다.';
-    btn.classList.add('btn-L--off');
-    pw.value = '';
+  } else {
+    document.getElementById("pwAlert").textContent =
+      "이메일이 또는 비밀번호가 일치하지 않습니다.";
+    btn.classList.add("btn-L--off");
+    pw.value = "";
   }
 }
 
-emailField.addEventListener('keyup', keyupDisabled);
-btn.addEventListener('click', login);
+emailField.addEventListener("keyup", keyupDisabled);
+btn.addEventListener("click", login);
