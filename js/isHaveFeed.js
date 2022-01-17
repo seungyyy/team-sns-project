f997191d535b1cc2874644ef96e5d90d7276a81a
seungyeon.lee <dus3214@gmail.com>
2022-01-17 12:54:01 +0900

    포스트 게시글 이미지 슬라이더 기능 구현

--- js/isHaveFeed.js
+++ js/isHaveFeed.js
@@ -1,9 +1,9 @@
 async function getFeed() {
-  const res = await fetch("http://146.56.183.55:5050/post/feed", {
-    method: "GET",
+  const res = await fetch('http://146.56.183.55:5050/post/feed?limit=15&skip=3', {
+    method: 'GET',
     headers: {
-      Authorization: `Bearer ${localStorage.getItem("token")}`,
-      "Content-type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem('token')}`,
+      'Content-type': 'application/json',
     },
   });
   const json = await res.json();
@@ -27,33 +27,71 @@ async function getFeed() {
     //게시물이 있을 경우 배열에서 게시물을 하나씩 꺼냄
     posts.forEach((post) => {
       //게시물 이미지로 들어온 소스를 구분한다. 이미지가 있으면 첫 이미지를 보여주고 이미지가 없으면 공백을 둔다.
-      let listImg
-      if(post.image){
-        if(post.image.split(",")[0].indexOf("http://146.56.183.55:5050/")>-1){
+      let listImg;
+      if (post.image) {
+        let imglink = post.image.split(',');
+        let imglength = post.image.split(',').length;
+        if (imglength === 1) {
           listImg = `
-          <div class="upload-imgBox">
-            <img src="${post.image.split(",")[0]}" alt="게시글 이미지" class="upload-img"/>
+          <div class="img-carousel">
+            <ul class="img-container">
+              <li class="upload-imgBox">
+                <img src="${imglink[0]}" onerror="this.src='http://146.56.183.55:5050/${imglink[0]}';"  alt="${post.username}님의 게시글 이미지">
+              </li>
+            <div>
           </div>
-          `
-        }else{
+          `;
+        } else if (imglength === 2) {
           listImg = `
-          <div class="upload-imgBox">
-            <img src="${"http://146.56.183.55:5050/" + post.image.split(",")[0]}" alt="게시글 이미지" class="upload-img"/>
+          <div class="img-carousel">
+            <ul class="img-container">
+              <li class="upload-imgBox">
+                <img src="${imglink[0]}" onerror="this.src='http://146.56.183.55:5050/${imglink[0]}';" alt="${post.username}님의 게시글 이미지">
+              </li>
+              <li class="upload-imgBox">
+                <img src="${imglink[1]}" onerror="this.src='http://146.56.183.55:5050/${imglink[1]}' alt="${post.username}님의 게시글 이미지">
+              </li>
+            </ul>
+            <div class="img-btn">
+              <button type="button" class="one-btn current"></button>
+              <button type="button" class="two-btn"></button>
+            </div>
           </div>
-          `
+          `;
+        } else if (imglength === 3) {
+          listImg = `
+          <div class="img-carousel">
+            <ul class="img-container">
+              <li class="upload-imgBox">
+                <img src="${imglink[0]}" onerror="this.src='http://146.56.183.55:5050/${imglink[0]}';" alt="${post.username}님의 게시글 이미지">
+              </li>
+              <li class="upload-imgBox">
+                <img src="${imglink[1]}" onerror="this.src='http://146.56.183.55:5050/${imglink[1]}';" alt="${post.username}님의 게시글 이미지">
+              </li>
+              <li class="upload-imgBox">
+                <img src="${imglink[2]}" onerror="this.src='http://146.56.183.55:5050/${imglink[2]}';" alt="${post.username}님의 게시글 이미지">
+              </li>
+            </ul>
+            <div class="img-btn">
+              <button type="button" class="one-btn current"></button>
+              <button type="button" class="two-btn"></button>
+              <button type="button" class="three-btn"></button>
+            </div>
+          </div>
+          `;
         }
-      }else{
+      } else {
         listImg = "<div class='post-cont-space'></div>";
       }
       //날짜 수정
-      let yearMonth = post.updatedAt.split("-");
-      let day = yearMonth[2].split("T")[0];
+      let yearMonth = post.updatedAt.split('-');
+      let day = yearMonth[2].split('T')[0];
       //하트 여부 확인
-      let heartImg = !post.hearted ? 
-      "../images/icon/icon-heart.png" :
-      "../images/icon/icon-heart-fill.png" ;
+      let heartImg = !post.hearted
+        ? '../images/icon/icon-heart.png'
+        : '../images/icon/icon-heart-fill.png';
       //가상 li요소를 만들고 내용을 채워넣음
-      let addItem = document.createElement("li");
+      let addItem = document.createElement('li');
       addItem.innerHTML = `
       <div href="../pages/profile.html" class="upload-user">
         <a href="../pages/otherProfile.html" class="upload-user-link">
@@ -82,33 +120,54 @@ async function getFeed() {
       <p class="upload-data">${yearMonth[0]}년 ${yearMonth[1]}월 ${day}일</p>
       </div>
     `;
-    //sec-upload ul에 li를 자식요소로 삽입한다.
-    feedList.prepend(addItem);
-    //하트 
-    document
-    .querySelector(".upload-btn-heart")
-    .addEventListener("click", () => {
-      if(!post.hearted){
-        heartPlus(post.id)
-      }else{
-        heartCancel(post.id)
-      }
-    });
-    //댓글 클릭시 게시글의 아이디를 전달한다.
-    document
-    .querySelector(".upload-btn-comment")
-    .addEventListener("click", () => {
-      localStorage.setItem("postId", post.id);
-    });
-    //유저 클릭시 해당 유저 프로필로 이동하면서 해당 유저의 accountname를 기억한다.
-    document
-    .querySelector(".upload-user-link")
-    .addEventListener("click", () => {
-      localStorage.setItem("postuploder", post.author.accountname);
-    });
-    modalDeclaration(post.id);
-  });
-  };
+      //sec-upload ul에 li를 자식요소로 삽입한다.
+      feedList.prepend(addItem);
+      //하트
+      document.querySelector('.upload-btn-heart').addEventListener('click', () => {
+        if (!post.hearted) {
+          heartPlus(post.id);
+        } else {
+          heartCancel(post.id);
+        }
+      });
+      //댓글 클릭시 게시글의 아이디를 전달한다.
+      document.querySelector('.upload-btn-comment').addEventListener('click', () => {
+        localStorage.setItem('postId', post.id);
+      });
+      //유저 클릭시 해당 유저 프로필로 이동하면서 해당 유저의 accountname를 기억한다.
+      document.querySelector('.upload-user-link').addEventListener('click', () => {
+        localStorage.setItem('postuploder', post.author.accountname);
+      });
+      modalDeclaration(post.id);
 
+      const slidercontainers = document.querySelectorAll('.img-carousel');
+      //const imgCont = document.querySelectorAll('.img-container');
+      slidercontainers.forEach((slider) => {
+        slider.addEventListener('click', (e) => {
+          const imgContainer = slider.querySelector('.img-container');
+          let oneBtn = slider.querySelector('.one-btn');
+          let twoBtn = slider.querySelector('.two-btn');
+          let threeBtn = slider.querySelector('.three-btn');
+          if (e.target.classList.contains('one-btn')) {
+            imgContainer.style.transform = `translateX(0)`;
+            oneBtn.classList.add('current');
+            twoBtn.classList.remove('current');
+            threeBtn.classList.remove('current');
+          } else if (e.target.classList.contains('two-btn')) {
+            imgContainer.style.transform = `translateX(-304px)`;
+            oneBtn.classList.remove('current');
+            threeBtn.classList.remove('current');
+            twoBtn.classList.add('current');
+          } else if (e.target.classList.contains('three-btn')) {
+            imgContainer.style.transform = `translateX(-608px)`;
+            oneBtn.classList.remove('current');
+            twoBtn.classList.remove('current');
+            threeBtn.classList.add('current');
+          }
+        });
+      });
+    });
+  };
 };
 getFeed();
+
